3df5d06369fe8fe111d2e7fa231a5a3e09d142a3
vicky0700018 <vs2734514@gmail.com>
2026-01-30 20:37:43 +0530

    updated

---
+++ app/Models/Order.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_amount',
+        'status',
+        'payment_method',
+        'stripe_payment_id',
+        'payment_details',
+    ];
+
+    protected $casts = [
+        'payment_details' => 'json',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
