7e3b1bb78dc0a39b52d76379140f12a543ca6fa8
vicky0700018 <vs2734514@gmail.com>
2026-05-27 22:09:03 +0530

    updated

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('₹'),
+                TextInput::make('image_url')
+                    ->url(),
+                TextInput::make('stock')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                TextInput::make('category'),
+            ]);
+    }
+}
