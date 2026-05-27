a06662bbac30c477876a7fd06209a22e3829b6a5
vicky0700018 <vs2734514@gmail.com>
2026-05-27 22:09:31 +0530

    updated

---
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class ProductInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('description')
+                    ->placeholder('-')
+                    ->columnSpanFull(),
+                TextEntry::make('price')
+                    ->money('inr'),
+                ImageEntry::make('image_url')
+                    ->height(200)
+                    ->placeholder('-'),
+                TextEntry::make('stock')
+                    ->numeric(),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('category')
+                    ->placeholder('-'),
+            ]);
+    }
+}
